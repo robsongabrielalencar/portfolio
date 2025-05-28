d892e45bfac5625f147693294991c5bc5bc61e98
Gabriel Alencar <gabrielrps89@gmail.com>
2025-05-28 20:10:07 -0300

    fix: pages dos projetos

--- app/page.tsx
+++ app/page.tsx
@@ -545,9 +545,11 @@ export default function Portfolio() {
                         ))}
                       </div>
                       <div className="flex space-x-2">
-                        <Button size="sm" variant="ghost" className="p-2">
-                          <Github className="w-4 h-4" />
-                        </Button>
+                        <a href={project.github} target="_blank" rel="noopener noreferrer">
+                            <Button size="sm" variant="ghost" className="p-2">
+                              <Github className="w-4 h-4" />
+                            </Button>
+                          </a>
                         <Button size="sm" variant="ghost" className="p-2">
                           <ExternalLink className="w-4 h-4" />
                         </Button>
